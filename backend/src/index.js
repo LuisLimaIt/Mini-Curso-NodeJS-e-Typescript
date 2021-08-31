447e8be6e736eefecbfd0a659a4a87aa2f85c454
LuisLimaIt <pls.luis@gmail.com>
2021-08-31 12:24:02 -0300

    Aula 6 - Parâmetros de requisições

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+// http://localhost:3333/projects?title=Node&owner=Aluizio
 
 app.get('/projects', (request, response) => {
+    const { title } = request.query;
+
+    console.log(title);
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -11,6 +17,9 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects',(request, response) => {
+    const body = request.body;
+
+    console.log(body);
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -19,7 +28,11 @@ app.post('/projects',(request, response) => {
     ]);
 });
 
-app.put('/projects', (request, response) => {
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+
+    console.log(id);
+
     return response.json([
         'Projeto 5',
         'Projeto 2',
@@ -28,7 +41,7 @@ app.put('/projects', (request, response) => {
     ]);
 });
 
-app.delete('/projects', (request, response) => {
+app.delete('/projects/:id', (request, response) => {
     return response.json([
         'Projeto 5',
         'Projeto 2', 
